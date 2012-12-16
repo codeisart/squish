f104d41a4be9c7e5f8dcfb0663eecf6e793ff220
Jimmy Smith <Jimmy@arya.local>
2012-12-16 16:03:38 +0000

    Unit test suite and all tests working. Added BumpCode

---
+++ Compress/Squish.cpp
@@ -0,0 +1,13 @@
+#include "BasicTypes.h"
+#include "BitIO.h"
+#include "Codec.h"
+#include "Lzw.h"
+
+#include <memory>
+#include <iostream>
+#include <exception>
+
+int main(int argc, char** argv)
+{
+	return 0;
+}
