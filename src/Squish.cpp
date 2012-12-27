c9f1675914d45c10cedfa2600c28a3fcb59d68e7
Jimmy Smith <jimmy@doomkeeper.com>
2012-12-27 11:47:15 +0000

    all commands working in basic form

--- src/Squish.cpp
+++ src/Squish.cpp
@@ -21,9 +21,10 @@ struct SquishState
 {
 	std::vector<std::string> files;
 	std::shared_ptr<ICommand> command;
+	std::string target;
 };
 
-class ListContentsCommand : public ICommand
+class ListCommand : public ICommand
 {
 public:
 	virtual bool execute(const SquishState& state) 
@@ -40,6 +41,57 @@ public:
 		return true;
 	}
 };
+
+
+class AddCommand : public ICommand
+{
+public:
+	virtual bool execute(const SquishState& state) 
+	{
+		if(state.target.empty())
+		{
+			std::cout << " add requires a target archive" << std::endl;
+			return false;	
+		}
+
+		auto w = std::shared_ptr<BitWriter>(new BitFileWriter(state.target.c_str()));
+		auto a = Archive::create(w);
+		std::cout << "creating " << state.target << std::endl;
+		for(auto i:state.files)
+		{
+			std::cout << "adding [" << i << "]" << std::endl;
+			auto r = std::shared_ptr<BitReader>(new BitFileReader(i.c_str()));
+			a->add(i,r.get());
+		}
+		return true;
+	}
+};
+
+
+class ExtractCommand : public ICommand
+{
+public:
+	virtual bool execute(const SquishState& state) 
+	{
+		for(auto i:state.files)
+		{
+			std::cout << "[" << i << "]" << std::endl;
+			auto f = std::shared_ptr<BitReader>(new BitFileReader(i.c_str()));
+			auto a = Archive::load(f);
+			auto c = a->contents();
+			
+			for(auto j:c)
+			{
+				std::cout << "extracting: " << j << std::endl;
+				auto w = std::shared_ptr<BitWriter>(new BitFileWriter(j.c_str()));
+				a->get(j,w);
+
+			}
+		}
+		return true;
+	}
+};
+
 std::shared_ptr<ICommand> processCommand(std::string& cmd)
 {
 	if(cmd.size() < 2)
@@ -50,24 +102,13 @@ std::shared_ptr<ICommand> processCommand(std::string& cmd)
 	switch(cmd[1])
 	{
 		// extract.
-		case 'x':
-		{
-			std::cout << "not yet implemented... " << std::endl;
-			return nullptr;
-		}
+		case 'x': return std::shared_ptr<ICommand>(new ExtractCommand());
 	
 		// add.
-		case 'a':
-		{
-			std::cout << "not yet implemented... " << std::endl;
-			return nullptr;
-		}
+		case 'a': return std::shared_ptr<ICommand>(new AddCommand());
 
 		// list contents.
-		case 'l':
-		{
-			return std::shared_ptr<ICommand>(new ListContentsCommand());
-		}
+		case 'l': return std::shared_ptr<ICommand>(new ListCommand());
 
 		// error.
 		default:
@@ -93,6 +134,11 @@ bool processArgs(std::vector<std::string>& args, SquishState& state)
 			}
 
 			state.command = processCommand(i);
+		}
+		// Target file?
+		else if('=' == i[0] && i.size() >1)
+		{
+			state.target = i.substr(1,std::string::npos);
 		}
 		else
 		{		
